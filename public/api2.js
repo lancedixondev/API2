78ae6112a9e435d4ef419fa76ee215b398ce98fd
Lance Dixon <lancedixondev@gmail.com>
2021-10-07 19:56:01 -0500

    finished

---
+++ public/api2.js
@@ -0,0 +1,47 @@
+const key = 'MGRmQiyG3DkATaOmEdsXJE0AZ6Fn6zXH8YjtzlR0';
+
+
+function fetchresponse(date){
+    let baseURL = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + date + '&end_date=' + date + '&api_key=';
+    fetch(baseURL + key)
+        .then(response => {
+            return response.json();
+        }).then(json => {
+            display(json, date)
+            console.log(document.getElementsByClassName('today')[0])
+            document.getElementsByClassName("today")[0].innerHTML = json.near_earth_objects[date].length
+        })
+    
+        
+}
+
+
+function display(json, date){ 
+    
+    for(x = 0; x <= 4; x++){
+        let currentAsteroid = "ast" + x 
+        document.getElementById(currentAsteroid).innerHTML = ""
+        document.getElementById(currentAsteroid).innerHTML += "ID: " + json.near_earth_objects[date][x].id + " "
+        document.getElementById(currentAsteroid).innerHTML += "Size(Miles): " + Math.round(json.near_earth_objects[date][x].estimated_diameter.miles.estimated_diameter_max * 1000) / 1000 + " "
+        document.getElementById(currentAsteroid).innerHTML += "dangerous: " + json.near_earth_objects[date][x].is_potentially_hazardous_asteroid + " "
+
+        fetch(json.near_earth_objects[date][x].links.self)
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            let filter = json.close_approach_data.filter(obj => obj.close_approach_date == date)
+            let mph = filter[0].relative_velocity.miles_per_hour;
+            document.getElementById(currentAsteroid).innerHTML += "MPH" + " " + Math.floor(mph)
+        })
+        }
+}
+
+
+function submit(){
+    fetchresponse(document.getElementById("input").value)
+}
+
+
+
+
